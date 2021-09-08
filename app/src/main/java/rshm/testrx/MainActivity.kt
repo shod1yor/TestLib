c50f26f5db609e0c19bc3d31f11cf36767da71b6
Shodiyor <Shodiyorrahmatov@gmail.com>
2021-09-08 10:37:29 +0500

    Commit message

--- app/src/main/java/rshm/testrx/MainActivity.kt
+++ app/src/main/java/rshm/testrx/MainActivity.kt
@@ -2,10 +2,12 @@ package rshm.testrx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import rshm.mylibrary.MsgUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
